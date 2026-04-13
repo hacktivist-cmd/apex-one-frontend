5c9c2c47b4c8d8b223ef296f12e362a2a07013ff
hacktivist-cmd <hacktivist.hub@gmail.com>
2026-04-13 14:47:14 +0100

    Hardcode backend URL in axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = 'https://apex-one-backend.onrender.com/api';
+
 const api = axios.create({
-  baseURL: 'https://apex-one-backend.onrender.com/api',
+  baseURL: API_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
